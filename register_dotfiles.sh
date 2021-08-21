84cfc01a6894b17be8c7a583e16676d4387ff68f
Nick Baker <ndbaker1@outlook.com>
2021-08-21 13:17:14 -0500

    change git reference to anything on path

--- register_dotfiles.sh
+++ register_dotfiles.sh
@@ -3,7 +3,7 @@
 REPO_URL="git@github.com:ndbaker1/conf.git"
 
 [ -d $HOME/.dotfiles.git ] || git clone --bare $REPO_URL $HOME/.dotfiles.git
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
 dotfiles checkout
 dotfiles config --local status.showUntrackedFiles no
 
