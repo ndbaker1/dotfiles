b00d4d49f060e768906faf839cbf945071d7c90a
Nick Baker <ndbaker1@outlook.com>
2021-12-09 13:54:51 -0600

    added stepse for checkout as cumulative

--- register_dotfiles.sh
+++ register_dotfiles.sh
@@ -4,8 +4,8 @@ REPO_URL="https://github.com/ndbaker1/conf.git"
 
 [ -d $HOME/.dotfiles.git ] || git clone --bare $REPO_URL $HOME/.dotfiles.git
 alias dotfiles="git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
-dotfiles checkout
-dotfiles config --local status.showUntrackedFiles no
 
+dotfiles checkout && \
+dotfiles config --local status.showUntrackedFiles no && \
 echo "alias set."
 
