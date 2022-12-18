9f61aadefa2e1ed866aee6bd56e58775bf67cf90
Nick Baker <ndbaker1@outlook.com>
2022-12-18 02:03:55 -0800

    nvim.lua cleanup
    remove unused LSP actions, add format-on-save buffer, include global
    on_attach with rust-tools server

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -189,18 +189,10 @@ local on_attach = function(_, bufnr)
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('<leader>dws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[D]ynamic [W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-  -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
@@ -213,6 +205,7 @@ local on_attach = function(_, bufnr)
 
   -- shortcut to use formatting command
   nmap('F', ':Format<CR>', 'Format Document with LPS')
+  vim.cmd("autocmd BufWritePost * lua vim.lsp.buf.format()")
 end
 
 -- Inlay hints setup
@@ -230,6 +223,7 @@ require("rust-tools").setup {
   },
   server = {
     on_attach = function(c, b)
+      on_attach(c, b)
       ih.on_attach(c, b)
     end,
   },
@@ -308,4 +302,3 @@ require("gruvbox").setup({
 })
 vim.o.background = "dark"
 vim.cmd("colorscheme gruvbox")
-
