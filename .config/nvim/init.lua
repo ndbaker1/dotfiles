398a0aaf8140643ae02c8bf27342d96ce0e63c01
Nick Baker <ndbaker1@outlook.com>
2023-02-09 00:50:53 -0600

    Move from packer to Lazy

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,25 +42,27 @@ vim.keymap.set('n', '<leader>;', ':Buffers<CR>')
 -- terminal mode things
 vim.keymap.set('t', '<C-x><C-c>', '<C-\\><C-N>')
 
--- [[ Install packer ]]
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local is_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    is_bootstrap = true
-    vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-    vim.cmd [[packadd packer.nvim]]
+-- [[ Install Lazy ]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
+vim.opt.rtp:prepend(lazypath)
 
-require('packer').startup(function(use)
-    -- Package manager
-    use 'wbthomason/packer.nvim'
-
+require('lazy').setup({
     -- colorscheme
-    use "ellisonleao/gruvbox.nvim"
+    'ellisonleao/gruvbox.nvim',
 
-    use { -- LSP Configuration & Plugins
+    { -- LSP Configuration & Plugins
         'neovim/nvim-lspconfig',
-        requires = {
+        dependencies = {
             -- Automatically install LSPs to stdpath for neovim
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
@@ -73,46 +75,28 @@ require('packer').startup(function(use)
             -- Rust Inlay Hints
             'simrat39/rust-tools.nvim',
         },
-    }
+    },
 
-    use { -- Autocompletion
+    { -- Autocompletion
         'hrsh7th/nvim-cmp',
-        requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-    }
+        dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+    },
 
-    use { -- Highlight, edit, and navigate code
+    { -- Highlight, edit, and navigate code
         'nvim-treesitter/nvim-treesitter',
         run = function()
             pcall(require('nvim-treesitter.install').update { with_sync = true })
         end,
-    }
+    },
 
-    use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-    use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
+    'nvim-lualine/lualine.nvim', -- Fancier statusline
+    'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
     -- Fuzzy Finder (files, lsp, etc)
-    use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
+    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
 
-    -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
-    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
-
-    -- Add custom plugins to packer from /nvim/lua/custom/plugins.lua
-    local has_plugins, plugins = pcall(require, 'custom.plugins')
-    if has_plugins then
-        plugins(use)
-    end
-
-    if is_bootstrap then
-        require('packer').sync()
-    end
-end)
-
--- Automatically source and re-compile packer whenever you save this init.lua
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost', {
-    command = 'source <afile> | PackerCompile',
-    group = packer_group,
-    pattern = vim.fn.expand '$MYVIMRC',
+    -- Fuzzy Finder Algorithm which dependencies local dependencies to be built. Only load if `make` is available
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 },
 })
 
 -- Set lualine as statusline
@@ -313,5 +297,4 @@ require("gruvbox").setup({
     italic = false,
     contrast = "hard", -- can be "hard", "soft" or empty string
 })
-vim.o.background = "dark"
-vim.cmd("colorscheme gruvbox")
+vim.cmd.colorscheme("gruvbox")
