6c6ebbce571b26c2fce9e4c3064b7e237370031c
Nick Baker <ndbaker1@outlook.com>
2023-07-06 10:38:43 -0700

    update telescope with smart path display

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -144,7 +144,8 @@ require('telescope').setup {
                 ["<C-j>"] = actions.move_selection_next,
                 ["<C-k>"] = actions.move_selection_previous,
             }
-        }
+        },
+        path_display = { "smart" },
     }
 }
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
