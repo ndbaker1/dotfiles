d1c232c9312693538b15e43abf7bae6e5ed8973e
Nick Baker <ndbaker1@outlook.com>
2023-10-08 12:58:35 -0700

    configure noice and reorganize configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,8 +10,8 @@ vim.o.title = true
 vim.o.showcmd = true
 vim.o.hlsearch = false
 vim.o.scrolloff = 8
-vim.o.completeopt = 'menuone,noselect,noinsert,preview'
 vim.o.clipboard = 'unnamedplus'
+vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert', 'preview' }
 
 -- [[ Formatting ]]
 vim.o.tabstop = 4
@@ -22,6 +22,7 @@ vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.textwidth = 80
 vim.o.whichwrap = 'bs<>[]'
+vim.o.wrap = false
 
 -- [[ History ]]
 vim.o.swapfile = false
@@ -31,19 +32,12 @@ vim.o.undodir = vim.fn.expand('~/.config/nvim/undodir')
 
 -- [[ Keymaps ]]
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = vim.g.mapleader
 vim.keymap.set('n', '<leader>q', ':q<CR>', { noremap = false, desc = 'quit' })
 vim.keymap.set('n', '<leader>w', ':w<CR>', { noremap = false, desc = 'write file' })
 vim.keymap.set('n', '<leader><leader>', '<c-^>', { noremap = false, desc = 'switch to last buffer' })
 
-
-if vim.g.vscode then
-    -- dont load anything else other than configs if this is used as a backend vscode
-    return
-end
-
-
--- [[ Install Lazy ]]
+-- [[ Lazy.nvim ]]
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -67,7 +61,7 @@ require('lazy').setup({
         config = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
-            require('which-key').setup {}
+            require('which-key').setup()
         end,
     },
 
@@ -75,12 +69,9 @@ require('lazy').setup({
     {
         'neovim/nvim-lspconfig',
         dependencies = {
-            -- Automatically install LSPs to stdpath for neovim
-            {
-                'williamboman/mason.nvim',
-                build = ':MasonUpdate', -- :MasonUpdate updates registry contents
-            },
+            { 'williamboman/mason.nvim', build = ':MasonUpdate' },
             'williamboman/mason-lspconfig.nvim',
+
             'simrat39/inlay-hints.nvim', -- Inlay Hints
             'simrat39/rust-tools.nvim',  -- Rust Inlay Hints
         },
@@ -89,19 +80,43 @@ require('lazy').setup({
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
-        dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip',
+            { 'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup() end },
+        },
     },
 
     -- Fancier, non-blocking notifications
     {
-        "folke/noice.nvim",
-        event = "VeryLazy",
+        'folke/noice.nvim',
+        event = 'VeryLazy',
         dependencies = {
-            "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
+            'MunifTanjim/nui.nvim',
+            'rcarriga/nvim-notify',
         },
         config = function()
-            require("noice").setup({})
+            -- some settings for notify
+            require('notify').setup({
+                background_colour = '#000000', -- satisfy warning
+                stages = 'fade',
+            })
+            local telescope_notify = require('telescope').extensions.notify.notify
+            vim.keymap.set('n', '<leader>sn', telescope_notify, { desc = '[S]earch [N]otifications' })
+
+            require('noice').setup({
+                lsp = {
+                    override = {
+                        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                        ['vim.lsp.util.stylize_markdown'] = true,
+                    },
+                },
+                presets = {
+                    lsp_doc_border = true,         -- add a border to hover docs and signature help
+                    long_message_to_split = false, -- long messages will be sent to a split
+                }
+            })
         end
     },
 
@@ -123,11 +138,11 @@ require('lazy').setup({
     {
         'nvim-lualine/lualine.nvim',
         config = function() -- See `:help lualine.txt`
-            require('lualine').setup {
+            require('lualine').setup({
                 options = {
                     theme = 'catppuccin',
                 },
-            }
+            })
         end
     },
 
@@ -148,20 +163,24 @@ require('lazy').setup({
     },
 })
 
--- [[ Configure Treesitter ]]
+-- [[ Languages ]]
+local lsp_servers = { 'rust_analyzer', 'lua_ls', 'pyright', 'bashls' }
+local languages = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'toml', 'json' }
+
+-- [[ Treesitter ]]
 -- See `:help nvim-treesitter`
-require('nvim-treesitter.configs').setup {
+require('nvim-treesitter.configs').setup({
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'toml', 'json' },
+    ensure_installed = languages,
     highlight = { enable = true },
     indent = { enable = true },
-}
+})
 
--- [[ Configure Telescope ]]
+-- [[ Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
 local actions = require('telescope.actions')
 local builtins = require('telescope.builtin')
-require('telescope').setup {
+require('telescope').setup({
     defaults = {
         mappings = {
             i = {
@@ -173,7 +192,7 @@ require('telescope').setup {
         layout_strategy = 'vertical',
         path_display = { 'smart' },
     }
-}
+})
 vim.keymap.set('n', '<C-p>', builtins.find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', builtins.help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', builtins.grep_string, { desc = '[S]earch current [W]ord' })
@@ -182,8 +201,85 @@ vim.keymap.set('n', '<leader>sd', builtins.diagnostics, { desc = '[S]earch [D]ia
 vim.keymap.set('n', '<leader>so', builtins.oldfiles, { desc = '[S]earch [O]ld' })
 vim.keymap.set('n', '<leader>sb', builtins.builtin, { desc = '[S]earch [B]uiltins' })
 
+-- [[ Completion ]]
+local cmp = require('cmp')
+local luasnip = require('luasnip')
 
--- [[ LSP settings ]]
+local select_opts = { behavior = cmp.SelectBehavior.Insert, select = true }
+local cmp_modes = { 'i', 's' }
+local source_map = {
+    buffer = 'Ω',
+    luasnip = '⋗',
+    nvim_lsp = 'λ',
+    nvim_lsp_signature_help = 'λ',
+    path = '🖫',
+}
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    sources = (function()
+        local sources = {}
+        for key, _ in pairs(source_map) do table.insert(sources, { name = key }) end
+        return sources
+    end)(),
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    formatting = {
+        format = function(entry, item)
+            item.menu = source_map[entry.source.name]
+            return item
+        end
+    },
+    mapping = cmp.mapping.preset.insert {
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<c-j>'] = cmp.mapping(cmp.mapping.select_next_item(select_opts), cmp_modes),
+        ['<c-k>'] = cmp.mapping(cmp.mapping.select_prev_item(select_opts), cmp_modes),
+        ['<c-space>'] = cmp.mapping(function()
+            if not cmp.visible() then
+                cmp.complete()
+            else
+                cmp.select_next_item(select_opts)
+            end
+        end),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, cmp_modes),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, cmp_modes),
+    },
+})
+
+-- [[ Mason ]]
+require('mason').setup()
+-- Ensure the servers above are installed
+require('mason-lspconfig').setup({
+    -- add language servers that arent managed by mason
+    ensure_installed = lsp_servers,
+})
+
+-- [[ LSPConfig ]]
+local lspconfig = require('lspconfig')
+-- nvim-cmp supports additional completion capabilities
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 --  This function gets run when an LSP connects to a particular buffer.
 local on_attach = function(_, bufnr)
     -- In this case, we create a function that lets us more easily define mappings specific
@@ -196,22 +292,6 @@ local on_attach = function(_, bufnr)
         vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
     end
 
-    -- vscode-like LSP renaming interface
-    nmap('<F2>', vim.lsp.buf.rename, '[R]e[n]ame')
-
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-    nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-    nmap('<leader>dws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[D]ynamic [W]orkspace [S]ymbols')
-
-    -- See `:help K` for why this keymap
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-
     -- Create a command `:Format` local to the LSP buffer
     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
         if vim.lsp.buf.format then
@@ -220,82 +300,37 @@ local on_attach = function(_, bufnr)
             vim.lsp.buf.formatting()
         end
     end, { desc = 'Format current buffer with LSP' })
-
-    -- shortcut to use formatting command
-    nmap('F', ':Format<CR>', 'Format Document with LPS')
-
     vim.api.nvim_create_autocmd('BufWritePost', {
         pattern = '*',
         command = ':Format',
     })
+
+    nmap('<F2>', vim.lsp.buf.rename, 'Rename')
+    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+    nmap('gr', builtins.lsp_references, '[G]oto [R]eferences')
+    nmap('gd', builtins.lsp_definitions, '[G]oto [D]efinition')
+    nmap('<C-LeftMouse> <LeftMouse>', builtins.lsp_definitions, '[Mouse] Goto Definition')
+
+    nmap('<leader>da', vim.lsp.buf.code_action, '[D]o Code [A]ction')
+    nmap('<leader>ds', builtins.lsp_document_symbols, '[D]ocument [S]ymbols')
+    nmap('<leader>dws', builtins.lsp_dynamic_workspace_symbols, '[D]ynamic [W]orkspace [S]ymbols')
+    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+
+    -- shortcut to use formatting command
+    nmap('F', ':Format<CR>', 'Format Document with LPS')
 end
 
-
--- [[ Mason ]]
-require('mason').setup()
-
-local lsp_servers = { 'rust_analyzer', 'lua_ls', 'pyright' }
--- Ensure the servers above are installed
-require('mason-lspconfig').setup {
-    -- add language servers that arent managed by mason
-    ensure_installed = lsp_servers,
-}
-
--- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
--- [[ LSPConfig ]]
 for _, lsp in ipairs(lsp_servers) do
-    require('lspconfig')[lsp].setup {
+    lspconfig[lsp].setup({
         on_attach = on_attach,
         capabilities = capabilities,
-    }
+    })
 end
 
--- [[ Completion ]]
-local cmp = require('cmp')
-local luasnip = require('luasnip')
+-- [[ ETC ]]
 
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert {
-        ['<c-space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 's' }),
-        ['<CR>'] = cmp.mapping.confirm { -- 'Enter' on selected option replaces text
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = false,
-        },
-        ['<c-j>'] = cmp.mapping(cmp.mapping.select_next_item {
-            behavior = cmp.SelectBehavior.Insert
-        }, { 'i', 's' }),
-        ['<c-k>'] = cmp.mapping(cmp.mapping.select_prev_item {
-            behavior = cmp.SelectBehavior.Insert
-        }, { 'i', 's' }),
-        ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
-    },
-    sources = {
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-    },
-}
+if vim.g.vscode then
+    -- dont load anything else other than configs if this is used as a backend vscode
+    return
+end
