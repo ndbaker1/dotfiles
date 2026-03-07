dd6aa37a8f899f2744004b2f8134344f4f662402
Nick Baker <nbaker@coreweave.com>
2026-03-06 21:09:56 -0800

    feat: add kubernetes context to fish prompt

--- .config/fish/conf.d/kubernetes.fish
+++ .config/fish/conf.d/kubernetes.fish
@@ -10,6 +10,16 @@ if status is-interactive
 
   if command -v kubectl &> /dev/null
     abbr -a k 'kubectl'
+    
+    # TODO: i like this but i need to stardardize fish prompts better.
+    function fish_prompt
+        fish_prompt_original
+        if kubectl config current-context &> /dev/null
+            set_color FFBBBA
+            printf "%s" "(☁︎  $(kubectl config current-context))> "
+            set_color normal
+        end
+    end
 
     if command -v kubecolor &> /dev/null
       alias kubectl='kubecolor'
